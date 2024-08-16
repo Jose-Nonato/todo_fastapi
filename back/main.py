510e35b4b6b645108990f9482fe03d1ccdde3160
José Nonato <josnonato161@gmail.com>
2024-08-16 11:27:22 -0300

    feat: conexao com db e operacoes crud

--- back/main.py
+++ back/main.py
@@ -1,1 +1,52 @@
-print("Olá mundo!")
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+import models
+from bson.objectid import ObjectId
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+
+app = FastAPI()
+
+
+client = MongoClient(config["MONGO_CONNECT"])
+db = client[config["DB_NAME"]]
+collection = db[config["COLLECTION"]]
+
+
+@app.post("/books", response_model=models.Book)
+async def create_book(book: models.Book):
+    book_dict = book.model_dump()
+    collection.insert_one(book_dict)
+    return book
+
+
+@app.get("/books")
+async def get_books():
+    books = []
+    for book in collection.find():
+        books.append(models.Book(**book))
+    return books
+
+
+@app.get("/books/{book_id}", response_model=models.Book)
+async def get_book(book_id: str):
+    book = collection.find_one({"_id": ObjectId(book_id)})
+    if book:
+        return models.Book(**book)
+    else:
+        raise HTTPException(status_code=404, detail="Book not found")
+    
+
+@app.put("/books/{book_id}")
+async def update_book(book_id: str, book: models.Book):
+    book_dict = book.model_dump()
+    collection.update_one({"_id": ObjectId(book_id)}, {"$set": book_dict})
+    return book
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: str):
+    collection.delete_one({"_id": ObjectId(book_id)})
+    return {"message": "Book deleted successfully!"}
+
---
+++ back/models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+    description: str
+    published_year: int
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "The Hitchhiker's Guide to the Galaxy",
+                "author": "Douglas Adam",
+                "description": "A humorous science fiction novel.",
+                "published_year": 1979
+            }
+        }
